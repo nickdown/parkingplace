3036b5a99014e64055c3637d1ba189cbc4f2d982
Nick Down <nick@trafficstamp.com>
2018-11-17 09:47:40 -0500

    mutate eloquent properties to dates

--- app/Visit.php
+++ app/Visit.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Visit extends Model
 {
-    //
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        'starting_at',
+        'ending_at'
+    ];
 }

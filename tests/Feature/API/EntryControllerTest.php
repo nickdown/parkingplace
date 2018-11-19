ccdafde9cd34b388d2577c53933f7afd1ed8ac3a
Nick Down <nick@relayclick.com>
2018-11-19 00:48:16 -0500

    clean up tests

---
+++ tests/Feature/API/EntryControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature\API;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class EntryControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_new_user_can_store_an_entry_to_the_garage()
+    {
+        $user = factory('App\User')->create();
+
+        $this->assertDatabaseMissing('visits', [
+            'user_id' => $user->id
+        ]);
+
+        $this->actingAs($user)->json('POST', '/visits');
+
+        $this->assertDatabaseHas('visits', [
+            'user_id' => $user->id
+        ]);
+    }
+    
+    /** @test */
+    public function a_user_already_in_the_garage_cannot_store_an_entry_to_the_garage()
+    {
+        $user = factory('App\User')->create();
+
+        $user->enterGarage();
+
+        $this->actingAs($user)->json('POST', '/visits')->assertStatus(403);
+
+        $this->assertDatabaseHas('visits', [
+            'user_id' => $user->id
+        ]);
+    }
+}
--- tests/Feature/UserCanEnterTheGarageTest.php
+++ tests/Feature/UserCanEnterTheGarageTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use Exception;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -19,6 +20,18 @@ class UserCanEnterTheGarageTest extends TestCase
     }
 
     /** @test */
+    public function a_user_can_enter_the_garage()
+    {
+        $user = factory('App\User')->create();
+        $this->assertSame(0, $user->visits()->count());
+
+        $user->enterGarage();
+        $user->refresh();
+
+        $this->assertSame(1, $user->visits()->count());
+    }
+
+    /** @test */
     public function a_user_is_not_allowed_to_enter_the_garage_if_they_are_in_the_garage()
     {
         $user = factory('App\User')->create();
@@ -31,14 +44,10 @@ class UserCanEnterTheGarageTest extends TestCase
     /** @test */
     public function a_user_cannot_enter_garage_if_they_are_already_in_the_garage()
     {
+        $this->expectException(Exception::class);
         $user = factory('App\User')->create();
+        $user->enterGarage();
 
         $user->enterGarage();
-        
-        try {
-            $user->enterGarage();
-        } catch (\Exception $e) {
-            $this->assertTrue(true);
-        }
     }
 }

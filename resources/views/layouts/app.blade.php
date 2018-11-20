8c52a7dd090bc33cea451c6921ec4a82aedbf1d5
Nick Down <nick@relayclick.com>
2018-11-20 00:41:53 -0500

    set stripeKey as a global variable in javascript

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,11 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="https://checkout.stripe.com/checkout.js"></script>
+    <script>
+        var ParkingPlace = {
+            stripeKey: "{{ config('services.stripe.key') }}"
+        };
+    </script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">

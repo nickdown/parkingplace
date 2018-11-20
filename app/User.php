a49256e0fbb55bc22eefc7012bac5791f1e1726d
Nick Down <nick@relayclick.com>
2018-11-19 23:06:37 -0500

    add passport for accessing api routes in javascript frontend

--- app/User.php
+++ app/User.php
@@ -4,13 +4,14 @@ namespace App;
 
 use Exception;
 use App\Garage;
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.

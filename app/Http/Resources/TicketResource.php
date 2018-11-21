93a60b6a6fcd3d9f5c7bbd3d7fbdf859d1c6a4f6
Nick Down <nick@relayclick.com>
2018-11-20 20:06:29 -0500

    create resources

---
+++ app/Http/Resources/TicketResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TicketResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'user' => new UserResource($this->user),
+            'entered_at' => Carbon::make($this->entered_at)->toDayDateTimeString(),
+            'exited_at' => optional(Carbon::make($this->exited_at))->toDayDateTimeString(),
+            'paid_at' => $this->paid_at,
+            'paid_amount' => $this->paid_amount,
+            'isPaid' => $this->isPaid(),
+            'rate' => new RateResource($this->rate())
+        ];
+    }
+}

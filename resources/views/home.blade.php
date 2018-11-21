f829c4ea37c5b8a247a9ded47b968f544700b407
Nick Down <nick@relayclick.com>
2018-11-20 23:49:22 -0500

    show some info in the sidebar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,11 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 mb-3">
             <user-handler></user-handler>
         </div>
+
+        @include('layouts.sidebar')
     </div>
 </div>
 @endsection

0bb17ae02c64bff8ec83629c42a528c69c3dabf6
Nick Down <nick@relayclick.com>
2018-11-22 00:52:02 -0500

    clean up unused traits

--- app/Events/GateRaiseRequested.php
+++ app/Events/GateRaiseRequested.php
@@ -2,15 +2,9 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
 class GateRaiseRequested
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable;
 }
--- app/Listeners/RaiseGate.php
+++ app/Listeners/RaiseGate.php
@@ -3,8 +3,6 @@
 namespace App\Listeners;
 
 use App\Events\GateRaiseRequested;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
 class RaiseGate
 {
